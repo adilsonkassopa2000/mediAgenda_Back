af3a9a841bf2eb55013a32017fde6c4b3b0a9e50
AdilsonKassopa <AdilsonKassopa@gmail.com>
2026-07-13 23:46:15 +0100

    Allow frontend localhost origin in CORS

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 3001;
 const corsOption = {
     origin: [
         "http://localhost:5500",
+	"http://127.0.0.1:5500",
         "http://localhost:5173",
+	"http://127.0.0.1:5173",
         "https://teu-frontend.vercel.app"
     ],
     methods:[
